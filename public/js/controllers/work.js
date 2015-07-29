332640b546b60cf9fbc085b8854428138ded76cb
Adrian Enriquez <eydreeyawn@gmail.com>
2015-07-30 02:46:28 +0800

    updates

--- public/js/controllers/work.js
+++ public/js/controllers/work.js
@@ -15,9 +15,23 @@ angular.module('ae.work', ['ngAnimate'])
         return toSnakeCase($stateParams.permalink);
     };
 
+    function onAnimationEnd(elem, callback){
+        $(elem).one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', callback);
+    };
+
+    /** 
+     * Tweak to add animations to modal closing.
+     */
     $scope.closeModal = function(){
-        $scope.$dismiss();
-    }
+        var $modal = $('.modal');
+
+        $('.modal-backdrop').removeClass('in');
+        $modal.removeClass('in');
+        setTimeout(function(){
+            $scope.$dismiss();
+        }, 200);
+        
+    };
 
     $scope.showAllWorks = true;
     $scope.active = selectedWork();
